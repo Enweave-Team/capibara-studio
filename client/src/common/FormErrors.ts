dc163fce3332411aab1cec48d31a157501214087
VladyslavHodoniuk <godonyuk2001@gmail.com>
2023-04-20 22:59:12 +0200

    Almost fully completed registration page. v2

---
+++ client/src/common/FormErrors.ts
@@ -0,0 +1,9 @@
+const FormErrors = {
+    firstName: "First name is required",
+    lastName: 'Second name is required',
+    password:'Password is required',
+    email:'Email is required',
+
+};
+
+export default FormErrors;
